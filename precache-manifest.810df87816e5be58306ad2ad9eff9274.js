ba510e23c623ac91b0b1fae1af1fb265bd0354af
Noreen Ladha <ladha.noreen@gmail.com>
2019-01-28 09:31:03 -0700

    Updates

---
+++ precache-manifest.810df87816e5be58306ad2ad9eff9274.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "027667c63672ca393ce4",
+    "url": "/clicky-game/static/js/main.027667c6.chunk.js"
+  },
+  {
+    "revision": "a6363c63e6196dc4f53f",
+    "url": "/clicky-game/static/js/1.a6363c63.chunk.js"
+  },
+  {
+    "revision": "027667c63672ca393ce4",
+    "url": "/clicky-game/static/css/main.7c576917.chunk.css"
+  },
+  {
+    "revision": "df4618d5a8b25ad432ff34b24ef11c2b",
+    "url": "/clicky-game/index.html"
+  }
+];
